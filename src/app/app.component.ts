06f54311bf555b9f5dec364b7fa8cb233d458a2b
Shiva <skate_92@ymail.com>
2018-03-22 11:32:50 +0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,81 @@
 import { Component } from '@angular/core';
 
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database-deprecated';
+
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  realitem: any;
+  images1: any;
   title = 'app';
+
+  public ssb = [];
+
+  constructor(af: AngularFireDatabase) {
+
+  let alphas = [];
+
+  let realitem = [];
+
+    af.list('/assets/ssb', { preserveSnapshot: true })
+    .subscribe(snapshots=>{
+    
+        snapshots.forEach(snapshot => {
+
+          alphas.push(snapshot.val());
+
+        });
+
+        realitem.push(alphas[0]);
+
+        var asset2 = alphas[0].numplate;
+
+
+        for(var i=0; i<alphas.length; i++){
+
+        if(alphas[i].numplate != asset2){
+        realitem.push(alphas[i]);
+        asset2 = alphas[i].numplate;
+
+        }
+
+        }
+
+        this.realitem = realitem;
+
+    })
+
+    this.ssb = alphas;
+
+  }
+
+  viewimages(real){
+
+  let images1 = [];
+
+    for(var i=0; i<this.ssb.length; i++){
+
+    if(this.ssb[i].numplate == real.numplate){
+
+      images1.push(this.ssb[i]);
+
+    }
+
+    }
+
+    this.images1 = images1;
+
+    console.log(images1);
+
+    console.log(this.ssb);
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from 'angularfire2';
+
+import { AngularFireDatabaseModule } from 'angularfire2/database-deprecated';
+
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyDAI9gD-Kpxsk-km-YMa5JJts8TRS00ZCM",
+    authDomain: "myapp-176d4.firebaseapp.com",
+    databaseURL: "https://myapp-176d4.firebaseio.com",
+    projectId: "myapp-176d4",
+    storageBucket: "myapp-176d4.appspot.com",
+    messagingSenderId: "867690478367"
+};
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+
+  };
